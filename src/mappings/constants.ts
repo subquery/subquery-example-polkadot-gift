71f50309aab7d96381ef7a17d02ede0fac4ab854
Hao Zhai <zhaihaohk@gmail.com>
2021-10-15 11:45:33 +1300

    Fix bug for setting claim info

---
+++ src/mappings/constants.ts
@@ -0,0 +1,8 @@
+export enum GiftRemarkStatus {
+  // `gift::create`
+  CREATED = '0xa29b801235241a572e6fda5836a6bd530fa21ce81bbfe98062386e51fb92ed94',
+  // `gift::claim`
+  CLAIMED = '0x297c7313d5dccc8aa8b133b47bda0e61c7ca3da870d676972f5412b3a732c74e',
+  // `gift::remove`
+  REMOVED = '0x4053cb611af36c422c9d6914ca3bde8862deea45aad4be9c180044a27511e66a',
+}
--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,15 +1,7 @@
 import {SubstrateEvent, SubstrateExtrinsic} from "@subql/types";
 import { EventRecord, Balance } from '@polkadot/types/interfaces';
 import {Gift, GiftExtrinsic} from "../types";
-
-enum GiftStatus {
-  // `gift::create`
-  CREATED = '0xa29b801235241a572e6fda5836a6bd530fa21ce81bbfe98062386e51fb92ed94',
-  // `gift::claim`
-  CLAIMED = '0x297c7313d5dccc8aa8b133b47bda0e61c7ca3da870d676972f5412b3a732c74e',
-  // `gift::remove`
-  REMOVED = '0x4053cb611af36c422c9d6914ca3bde8862deea45aad4be9c180044a27511e66a',
-}
+import {GiftRemarkStatus} from './constants';
 
 function findTransferEvent(events: EventRecord[]): EventRecord | undefined {
     return events.find((event) => {
@@ -44,24 +36,29 @@ async function handleGiftClaimed(extrinsic: SubstrateExtrinsic) {
         const [from, to] = event.event.data;
 
         const gift = await Gift.get(from.toString());
-        gift.claimExtrinsic = extrinsicHash;
-        gift.claimer = to.toString();
-        await gift.save();
+        if (gift) {
+            gift.claimExtrinsic = extrinsicHash;
+            gift.claimer = to.toString();
+            await gift.save();
+        }
     }
 }
 
 export async function handleRemark(event: SubstrateEvent): Promise<void> {
     const { event: { data }, extrinsic } = event;
-    if (data.length < 2) return;
+    if (!extrinsic.success || data.length < 2) return;
 
     const [owner, remark] = data;
     switch (remark.toString()) {
-        case GiftStatus.CREATED:
+        case GiftRemarkStatus.CREATED:
             await handleGiftCreated(extrinsic);
-        case GiftStatus.CLAIMED:
+            return;
+        case GiftRemarkStatus.CLAIMED:
             await handleGiftClaimed(extrinsic);
-        case GiftStatus.REMOVED:
+            return;
+        case GiftRemarkStatus.REMOVED:
             await Gift.remove(owner.toString());
+            return;
         default:
             return;
     }
