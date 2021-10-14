d40381516a82bb7bc08f711722960428c2261909
Hao Zhai <zhaihaohk@gmail.com>
2021-10-15 08:15:24 +1300

    Add remove action

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -2,10 +2,14 @@ import {SubstrateEvent, SubstrateExtrinsic} from "@subql/types";
 import { EventRecord, Balance } from '@polkadot/types/interfaces';
 import {Gift, GiftExtrinsic} from "../types";
 
-// `gift::create`
-const GIFT_CREATED = '0xa29b801235241a572e6fda5836a6bd530fa21ce81bbfe98062386e51fb92ed94'; 
-// `gift::claim`
-const GIFT_CLAIMED = '0x297c7313d5dccc8aa8b133b47bda0e61c7ca3da870d676972f5412b3a732c74e';
+enum GiftStatus {
+  // `gift::create`
+  CREATED = '0xa29b801235241a572e6fda5836a6bd530fa21ce81bbfe98062386e51fb92ed94',
+  // `gift::claim`
+  CLAIMED = '0x297c7313d5dccc8aa8b133b47bda0e61c7ca3da870d676972f5412b3a732c74e',
+  // `gift::remove`
+  REMOVED = '0x4053cb611af36c422c9d6914ca3bde8862deea45aad4be9c180044a27511e66a',
+}
 
 function findTransferEvent(events: EventRecord[]): EventRecord | undefined {
     return events.find((event) => {
@@ -50,10 +54,15 @@ export async function handleRemark(event: SubstrateEvent): Promise<void> {
     const { event: { data }, extrinsic } = event;
     if (data.length < 2) return;
 
-    const remark = data[1];
-    if (remark.toString() === GIFT_CREATED) {
-        await handleGiftCreated(extrinsic);
-    } else if (remark.toString() === GIFT_CLAIMED) {
-        await handleGiftClaimed(extrinsic);
+    const [owner, remark] = data;
+    switch (remark.toString()) {
+        case GiftStatus.CREATED:
+            await handleGiftCreated(extrinsic);
+        case GiftStatus.CLAIMED:
+            await handleGiftClaimed(extrinsic);
+        case GiftStatus.REMOVED:
+            await Gift.remove(owner.toString());
+        default:
+            return;
     }
 }
