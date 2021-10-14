3b5d830fb8fcd73bf83235baad2a884c9f017f11
Hao Zhai <zhaihaohk@gmail.com>
2021-10-15 02:15:37 +1300

    Add extrinsic link with gift

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,6 +1,6 @@
 import {SubstrateEvent, SubstrateExtrinsic} from "@subql/types";
 import { EventRecord, Balance } from '@polkadot/types/interfaces';
-import {Gift} from "../types";
+import {Gift, GiftExtrinsic} from "../types";
 
 // `gift::create`
 const GIFT_CREATED = '0xa29b801235241a572e6fda5836a6bd530fa21ce81bbfe98062386e51fb92ed94'; 
@@ -18,19 +18,29 @@ async function handleGiftCreated(extrinsic: SubstrateExtrinsic) {
     const event = findTransferEvent(extrinsic.events);
     if (event) {
         const [creater, owner, amount] = event.event.data;
+        const extrinsicHash = extrinsic.extrinsic.hash.toString();
+
         const gift = new Gift(owner.toString());
         gift.creater = creater.toString();
         gift.address = owner.toString();
         gift.amount = (amount as Balance).toBigInt();
+        gift.ceateExtrinsic = extrinsicHash;
         await gift.save();
+
+        const giftExtrinsic = new GiftExtrinsic(extrinsicHash);
+        giftExtrinsic.giftID = owner.toString();
+        await giftExtrinsic.save();
     }
 };
 
 async function handleGiftClaimed(extrinsic: SubstrateExtrinsic) {
     const event = findTransferEvent(extrinsic.events);
     if (event) {
+        const extrinsicHash = extrinsic.extrinsic.hash.toString();
         const [from, to] = event.event.data;
+
         const gift = await Gift.get(from.toString());
+        gift.claimExtrinsic = extrinsicHash;
         gift.claimer = to.toString();
         await gift.save();
     }
